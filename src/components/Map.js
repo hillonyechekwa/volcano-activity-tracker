20453291ae6c8289a309e8279d5d8ee140344730
Hill Onyechekwa <hillonyechekwa@gmail.com>
2021-04-11 19:28:57 +0100

    deleted location info set up and styled markers and popus

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,30 +1,21 @@
-import { useState } from 'react'
-import ReactMapGL, {Marker} from 'react-map-gl'
-import Locationinfo from './Locationinfo'
+import { useState} from 'react'
+import ReactMapGL, {Marker, Popup} from 'react-map-gl'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faFire} from '@fortawesome/free-solid-svg-icons'
 
 
 export default function Map({eventData}) {
-
-    // const markers = eventData.map(ev => {
-    //     if (ev.categories[0] === 12) {
-    //         return <Marker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} onClick={() => setLocationinfo({
-    //             id: ev.id,
-    //             title: ev.title
-    //         })}/>
-                
-    //     }
-    //     return null
-    // })
+        
 
     const [viewport, setViewPort] = useState({
         longitude: -61.18,
         latitude: 13.33,
         width: 800,
         height: 500,
-        zoom: 10
+        zoom: 0
     })
 
-    const [locationinfo, setLocationinfo] = useState(null)
+    const [locationInfo, setLocationInfo] = useState(null)
 
     
     return (
@@ -33,11 +24,40 @@ export default function Map({eventData}) {
          {...viewport}
                 mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
                 onViewportChange={nextViewport => setViewPort(nextViewport)}
-                mapStyle='mapbox://styles/vulkan/cknd4rpo521qk17r08p0lkiyv'
-        >
-          
+                mapStyle='mapbox://styles/vulkan/cknd4mwej05a517nylw8hff4f'
+            >
+                {
+                    eventData.map(ev => {
+                        {/* Dukono volcano is an anomaly in the data set so i excluded it */}
+                        if (ev.categories[0].id === 12 && ev.title !== "Dukono Volcano, Indonesia") {
+                            return <Marker key={ev.id} latitude={ev.geometries[0].coordinates[1]} longitude={ev.geometries[0].coordinates[0]}>
+                                <button
+                                    style={{ background: "none", outline: "none", border: "none", cursor: "pointer" }}
+                                    onClick={e => {
+                                        e.preventDefault();
+                                        setLocationInfo(ev)}
+                                    }>
+                                    <FontAwesomeIcon icon={faFire} size="s" style={{ color:"orange" }}/>
+                                </button>
+                            </Marker>
+                        }
+                        return null
+                    })
+                }
+                {locationInfo ? (
+                    <Popup
+                        latitude={locationInfo.geometries[0].coordinates[1]}
+                        longitude={locationInfo.geometries[0].coordinates[0]}
+                        onClose={() => {
+                            setLocationInfo(null)
+                        }}
+                    >
+                        <div>
+                            <h2 styles={{fontSize: "1rem"}}>{locationInfo.title}</h2>
+                        </div>               
+                    </Popup>
+                ) : null}
             </ReactMapGL>
-            {/* {locationinfo && <Locationinfo info={locationinfo} />} */}
             </div>
     )
 }
